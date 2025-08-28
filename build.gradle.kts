68ab1fefd1d3613c2178681a43173f937ce90343
zongkx <32995330+zongkx@users.noreply.github.com>
2025-08-28 19:56:14 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.intellij.platform") version "2.7.2"
 }
 
 group = "com.zongkx"
