7ef846ced94daf56c7778d20f99a2cda754f6deb
zongkx <32995330+zongkx@users.noreply.github.com>
2025-08-28 20:36:06 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
     id("org.jetbrains.intellij.platform") version "2.7.2"
-    id("com.jetbrains.changelog") version "2.0.1"
     id("maven-publish") // 添加 maven-publish 插件
 }
 
@@ -17,22 +16,13 @@ repositories {
     }
 }
 
-// Configure IntelliJ Platform Gradle Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
         create("IC", "2025.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
-
-        // Add necessary plugin dependencies for compilation here, example:
-        // bundledPlugin("com.intellij.java")
     }
-    // OkHttp 是一个现代的、高效的 HTTP 客户端
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
-    // Rome是一个用于解析RSS和Atom Feed的库
     implementation("com.rometools:rome:1.18.0")
-    // Rome依赖JDom，它是一个XML解析库
     implementation("com.rometools:rome-modules:1.18.0")
 }
 
